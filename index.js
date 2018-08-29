4a640768b279a7a1ce75ecf17d6f007d6bdeb864
Leslie Turis <lturis@gmail.com>
2018-08-29 17:21:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }else {
-    cart.length = 0;
+    cart = [];
     return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
   }
 }
